6b02fc8b09cdebb5c198e0380fdc477b236a704f
QuemeraT <adrianortizzt1vl@gmail.com>
2026-03-25 08:45:32 -0300

    feat: loading component fixed

--- frontend/src/components/common/Spinner.tsx
+++ frontend/src/components/common/Spinner.tsx
@@ -1,11 +1,10 @@
-import { LoaderCircle } from "lucide-react";
 import { Spin } from "antd";
 
 const Loading = () => {
   return (
     <Spin
-      spinning={true}
       styles={{ indicator: { color: "#6fcf97" } }}
+      size="large"
       fullscreen
     />
   );
