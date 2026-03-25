d9f5192c8b650005576513f86966655f6576b0ab
QuemeraT <adrianortizzt1vl@gmail.com>
2026-03-25 13:39:40 -0300

    fix: cache error fixed

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -12,4 +12,20 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("habitzz_token");
+      localStorage.removeItem("habitzz_user");
+
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  },
+);
+
 export default api;
