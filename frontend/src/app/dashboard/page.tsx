736becddbaf84fe0ab54fbbcae753f33a047b3bc
QuemeraT <adrianortizzt1vl@gmail.com>
2026-04-21 10:10:18 -0300

    fix: styles fixed

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -19,7 +19,7 @@ export default function HomePage() {
       <DashboardHeader setIsModalOpen={setIsModalOpen} />
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-        <div className="lg:col-span-3 space-y-8">
+        <div className="col-span-3 space-y-8">
           <WeeklyProgress summary={data?.summary} isLoading={isLoading} />
         </div>
         <div className="col-span-3">
